1b6ffc4abafbe1a31a1cd060de426ad1b980385a
Marcus Correa <mrc.correa@gmail.com>
2019-03-02 09:43:03 -0300

    Fixed solution.

---
+++ challenges/pyramid.js
@@ -0,0 +1,23 @@
+const staircase = n => {
+  let myString = ''
+  let verticalCounter = n
+
+  function drawLine(number) {
+    let horizontalCounter = 0
+    let lineDrawing = ''
+    while (horizontalCounter <= n) {
+      lineDrawing += horizontalCounter < number ? ' ' : '# '
+      horizontalCounter++
+    }
+    myString += lineDrawing + ' \n'
+  }
+
+  while (verticalCounter >= 0) {
+    drawLine(verticalCounter)
+    verticalCounter--
+  }
+
+  return myString
+}
+
+console.log(staircase(3))
