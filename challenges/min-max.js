53de4a32346e85b3f536b2411e3eccce36340ebf
Marcus Correa <mrc.correa@gmail.com>
2019-03-02 11:58:20 -0300

    Added min-max test.

---
+++ challenges/min-max.js
@@ -0,0 +1,13 @@
+function miniMaxSum(arr) {
+  let minimal = 0
+  let maximal = 0
+
+  arr.sort().map((item, index) => {
+    if (index < 4) minimal += item
+    if (index >= arr.length - 4) maximal += item
+  })
+
+  console.log(`${minimal} ${maximal}`)
+}
+
+miniMaxSum([1, 2, 3, 4, 5])
