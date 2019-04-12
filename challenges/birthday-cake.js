af79eb754259524a04e04d731816c69f4f07dbaf
Marcus Correa <mrc.correa@gmail.com>
2019-04-12 15:01:55 -0300

    Birthday cake

---
+++ challenges/birthday-cake.js
@@ -0,0 +1,11 @@
+const birthdayCakeCandles = (ar) => {
+
+  const tallestNumber = Math.max(...ar)
+  let tallestCount = 0
+
+  ar.map(item => item == tallestNumber ? tallestCount++ : '')
+
+  return tallestCount
+}
+
+console.log(birthdayCakeCandles([1, 2, 4, 4]))
