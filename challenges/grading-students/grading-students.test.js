14a5a52bbc6cb13601995275b23907ebe75b01e0
Marcus Correa <mrc.correa@gmail.com>
2019-04-13 16:45:46 -0300

    Organized folder

--- challenges/grading-students/grading-students.test.js
+++
@@ -1,7 +0,0 @@
-import { gradingStudents } from './grading-students'
-
-describe('Grading students function', () => {
-  it('Should return gradingStudents', () => {
-    expect(gradingStudents(1)).toEqual(true)
-  })
-})
